7e7de9bb0b17070f323dd03fd75f9b784061499d
yxzhan <zhanyanxiang@hotmail.com>
2026-03-20 20:06:16 +0000

    update testing notebook

--- binder/entrypoint.sh
+++ binder/entrypoint.sh
@@ -4,7 +4,7 @@
 source ${ROS_PATH}/setup.bash
 
 # Add other startup programs here
-
+ros2 launch rosbridge_server rosbridge_websocket_launch.xml &
 
 # The following should be at the end of the entrypoint. Don't modify it!!!
 exec "$@"
--- notebooks/utils.py
+++ notebooks/utils.py
@@ -3,7 +3,7 @@ from IPython.display import display, HTML
 from sidecar import Sidecar
 import ipywidgets as widgets
 
-def display_desktop(anchor="split-right"):
+def display_desktop(sub_path="desktop", in_sidecar=True, anchor="split-right", width="100%", height="100%"):
     """
     Display the remote desktop in a JupyterLab Sidecar tab.
     
@@ -12,6 +12,44 @@ def display_desktop(anchor="split-right"):
                     'split-right', 'split-left', 'split-top', 'split-bottom',
                     'tab-before', 'tab-after'
     """
+    url_prefix = get_base_url
+    remote_desktop_url = f"{url_prefix}/{sub_path}"
+
+    html_wrapper = HTML(f"""
+        <style>
+        body.p-mod-override-cursor div.iframe-widget {{
+            position: relative;
+            pointer-events: none;
+        }}
+
+        body.p-mod-override-cursor div.iframe-widget:before {{
+            content: '';
+            position: absolute;
+            top: 0;
+            left: 0;
+            right: 0;
+            bottom: 0;
+            background: transparent;
+        }}
+        </style>
+        <div class="iframe-widget" style="width: calc({width});height:{height};">
+            <iframe src="{remote_desktop_url}" width="100%" height="100%" style="border:0;"></iframe>
+        </div>
+
+    """)
+    if in_sidecar:
+        sc = Sidecar(title='Desktop', anchor=anchor)
+        with sc:
+            # The inserted custom HTML and CSS snippets are to make the tab resizable
+            display(html_wrapper)
+    else:
+        display(html_wrapper)
+
+    display(widgets.HTML(
+        value=f'<a href="{remote_desktop_url}"  class="jupyter-button" style="color: #fff;background-color: #1976d2;" target="_blank">Open Desktop in new Tab</a>',
+    ))
+
+def get_base_url():
     try:
         jupyterhub_user = os.environ["JUPYTERHUB_USER"]
         domain_name = os.environ["BINDER_LAUNCH_HOST"]
@@ -19,35 +57,5 @@ def display_desktop(anchor="split-right"):
     except KeyError:
         jupyterhub_user = None
         domain_name = "http://localhost:8888"
-    url_prefix = f"{domain_name}/user/{jupyterhub_user}" if jupyterhub_user is not None else ''
+    return f"{domain_name}user/{jupyterhub_user}" if jupyterhub_user is not None else ''
 
-    remote_desktop_url = f"{url_prefix}/desktop"
-
-    display(widgets.HTML(
-        value=f'<a href="{remote_desktop_url}"  class="jupyter-button" style="color: #fff;background-color: #1976d2;" target="_blank">Open Desktop in new Tab</a>',
-    ))
-    
-    sc = Sidecar(title='Desktop', anchor=anchor)
-    with sc:
-        # The inserted custom HTML and CSS snippets are to make the tab resizable
-        display(HTML(f"""
-            <style>
-            body.p-mod-override-cursor div.iframe-widget {{
-                position: relative;
-                pointer-events: none;
-            }}
-
-            body.p-mod-override-cursor div.iframe-widget:before {{
-                content: '';
-                position: absolute;
-                top: 0;
-                left: 0;
-                right: 0;
-                bottom: 0;
-                background: transparent;
-            }}
-            </style>
-            <div class="iframe-widget" style="width: calc(100% + 10px);height:100%;">
-                <iframe src="{remote_desktop_url}" width="100%" height="100%"></iframe>
-            </div>
-        """))
